e98a1d70f817018a0622987e09d8f628a45ca140
Colin Woodbury <colin@fosskers.ca>
2019-12-12 13:33:26 -0800

    Ideal `config.yaml` settings

--- installnode.sh
+++ installnode.sh
@@ -20,7 +20,6 @@ error() {
 }
 trap 'error ${LINENO}' ERR
 
-
 clear
 
 cat <<'FIG'
@@ -48,63 +47,41 @@ FIG
 
 # Check if executed as root user
 if [[ $EUID -ne 0 ]]; then
-	echo -e "This script has to be run as \033[1mroot\033[0m user."
-	exit 1
+  echo -e "This script has to be run as \033[1mroot\033[0m user."
+  exit 1
 fi
 
 
 # Print variable on a screen
 decho "Make sure you double check information before hitting enter!"
 
-read -e -p "Please enter the domain where the Kadena server will run: " whereami
-if [[ "$whereami" == "" ]]; then
-	decho "WARNING: No domain entered, exiting!!!"
-	exit 3
+read -e -p "Please enter your node's Domain Name: " whereami
+if [[ $whereami == "" ]]; then
+    decho "WARNING: No domain entered, exiting!"
+    exit 3
+fi
+
+read -e -p "Please enter your Public Key: " publickey
+if [[ $publickey == "" ]]; then
+    decho "WARNING: No public key given, exiting!"
+    exit 3
 fi
 
 # Check for systemd
-systemctl --version >/dev/null 2>&1 || { decho "systemd is required. Are you using Ubuntu 18.04?"  >&2; exit 1; }
-
-
-# Install swap
-decho "Enabling a swap partition..." 
-
-if free | awk '/^Swap:/ {exit !$2}'; then
-	decho "Has swap..."
-else
-	touch /var/swap.img
-	chmod 600 /var/swap.img
-	dd if=/dev/zero of=/var/swap.img bs=1024k count=2048
-	mkswap /var/swap.img
-	swapon /var/swap.img
-	echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
-fi
-
+systemctl --version >/dev/null 2>&1 || { decho "systemd is required. Are you using Ubuntu 18.04?" >&2; exit 1; }
 
 # Update packages
-decho "Updating system..."   
+decho "Updating system..."
 
 apt-get update -y >> $LOG_FILE 2>&1
-dpkg --configure -a
 
 # Install required packages
 decho "Installing base packages and dependencies..."
 decho "This may take a while..."
 
 apt-get install -y certbot >> $LOG_FILE 2>&1
-apt-get install -y librocksdb5.8  >> $LOG_FILE 2>&1
-apt-get install -y curl  >> $LOG_FILE 2>&1
-
-decho "Installing ufw..."
-apt-get install -y ufw >> $LOG_FILE 2>&1
-ufw allow ssh/tcp >> $LOG_FILE 2>&1
-ufw allow sftp/tcp >> $LOG_FILE 2>&1
-ufw allow 80/tcp >> $LOG_FILE 2>&1
-ufw allow 443/tcp >> $LOG_FILE 2>&1
-ufw default deny incoming >> $LOG_FILE 2>&1
-ufw default allow outgoing >> $LOG_FILE 2>&1
-ufw logging on >> $LOG_FILE 2>&1
-ufw --force enable >> $LOG_FILE 2>&1
+apt-get install -y librocksdb-dev >> $LOG_FILE 2>&1
+apt-get install -y curl >> $LOG_FILE 2>&1
 
 decho "Create user kda (if necessary)"
 
@@ -113,22 +90,25 @@ trap '' ERR
 getent passwd kda > /dev/null 2&>1
 
 if [ $? -ne 0 ]; then
-	trap 'error ${LINENO}' ERR
-	adduser --disabled-password --gecos "" kda >> $LOG_FILE 2>&1
+  trap 'error ${LINENO}' ERR
+  adduser --disabled-password --gecos "" kda >> $LOG_FILE 2>&1
 else
-	trap 'error ${LINENO}' ERR
+  trap 'error ${LINENO}' ERR
 fi
 
 # Download Node
+NODE=https://github.com/kadena-io/chainweb-node/releases/download/1.3.1/chainweb.8.6.5.ubuntu-18.04.1e6c76b2.tar.gz
+MINER=https://github.com/kadena-io/chainweb-miner/releases/download/v1.0.3/chainweb-miner-1.0.3-ubuntu-18.04.tar.gz
+
 decho 'Downloading Node...'
 cd /home/kda/
-wget --no-check-certificate https://github.com/kadena-io/chainweb-node/releases/download/1.3.1/chainweb.8.6.5.ubuntu-18.04.1e6c76b2.tar.gz >> $LOG_FILE 2>&1
+wget --no-check-certificate $NODE >> $LOG_FILE 2>&1
 tar -xvf chainweb.8.6.5.ubuntu-18.04.1e6c76b2.tar.gz >> $LOG_FILE 2>&1
-wget --no-check-certificate https://github.com/kadena-io/chainweb-miner/releases/download/v1.0.3/chainweb-miner-1.0.3-ubuntu-18.04.tar.gz >> $LOG_FILE 2>&1
+wget --no-check-certificate $MINER >> $LOG_FILE 2>&1
 tar -xvf chainweb-miner-1.0.3-ubuntu-18.04.tar.gz >> $LOG_FILE 2>&1
 
 # Create config.yaml
-decho "Creating config files and Health check..." 
+decho "Creating config files and Health check..."
 
 touch /home/kda/config.yaml
 cat << EOF > /home/kda/config.yaml
@@ -139,8 +119,8 @@ chainweb:
 
   throttling:
     local: 0.1
-    mining: 100000
-    global: 400
+    mining: 5
+    global: 50
     putPeer: 11
 
   mining:
@@ -148,23 +128,17 @@ chainweb:
     coordination:
       enabled: true
       # "public" or "private".
-      mode: public
+      mode: private
       # The number of "/mining/work" calls that can be made in total over a 5
       # minute period.
-      limit: 102400
-      # The number of work requests per client per second.
-      mining: 100000
+      limit: 1200
       # When "mode: private", this is a list of miner account names who are
       # allowed to have work generated for them.
-      miners:
-      - account: abc123
+      miners: []
+      - account: $publickey
         predicate: keys-all
         public-keys:
-        - 3438e5bcfd086c5eeee1a2f227b7624df889773e00bd623babf5fc72c8f9aa63
-      - account: cfd7816f15bd9413e5163308e18bf1b13925f3182aeac9b30ed303e8571ce997
-        predicate: keys-all
-        public-keys:
-        - cfd7816f15bd9413e5163308e18bf1b13925f3182aeac9b30ed303e8571ce997
+        - $publickey
 
   p2p:
     # Your node's network identity.
@@ -187,71 +161,15 @@ chainweb:
     # These will share more peers and block data to your Node.
     peers:
       - address:
-          hostname: akami.chainweb.tech
-          port: 443
-        id: null
-      - address:
-          hostname: arboretum.tech
-          port: 443
-        id: null
-      - address:
-          hostname: chainweb.xyz
-          port: 443
-        id: null
-      - address:
-          hostname: tsundere.waifuwars.org
-          port: 35090
-        id: null
-      - address:
-          hostname: kadena1.block77.io
-          port: 443
-        id: null
-      - address:
-          hostname: kadena2.block77.io
-          port: 443
-        id: null
-      - address:
-          hostname: ponzu.banteg.xyz
-          port: 1337
-        id: null
-      - address:
-          hostname: dumpling.banteg.xyz
-          port: 1337
-        id: null
-      - address:
-          hostname: sg.blockventur.es
-          port: 44444
-        id: null
-      - address:
-          hostname: sg.kadena.asymmetry.ventures
-          port: 44444
-        id: null
-      - address:
-          hostname: kadena.wayi.cn
-          port: 443
-        id: null
-      - address:
-          hostname: kadenamerkletree.com
-          port: 443
-        id: null
-      - address:
-          hostname: chungle.constant.gripe
-          port: 1343
-        id: null
-      - address:
-          hostname: fr1.chainweb.com
-          port: 443
-        id: null
-      - address:
-          hostname: pn.hyperioncn.net
-          port: 443
-        id: null
-      - address:
-          hostname: cw.hyperioncn.net
-          port: 443
-        id: null
-      - address:
           hostname: us-w1.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: us-w2.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: us-w3.chainweb.com
           port: 443
         id: null
       - address:
@@ -259,11 +177,35 @@ chainweb:
           port: 443
         id: null
       - address:
-          hostname: jp1.chainweb.com
+          hostname: us-e2.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: us-e3.chainweb.com
           port: 443
         id: null
       - address:
           hostname: fr1.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: fr2.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: fr3.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: jp1.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: jp2.chainweb.com
+          port: 443
+        id: null
+      - address:
+          hostname: jp3.chainweb.com
           port: 443
         id: null
 
@@ -372,7 +314,7 @@ systemctl start node.service
 sleep 10
 systemctl stop node.service
 
-#Download recent Bootstrap......" 
+#Download recent Bootstrap......"
 echo "Downloading recent Bootstrap..."
 echo "This may take a while..."
 
