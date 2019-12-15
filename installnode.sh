ee477e5e2c55eee0e4cd0c8daf7c51baacaed9ac
TaonaProject <50932604+TaonaProject@users.noreply.github.com>
2019-12-15 10:54:12 -0500

    Update node 1.4

--- installnode.sh
+++ installnode.sh
@@ -93,14 +93,14 @@ apt-get install -y curl >> $LOG_FILE 2>&1
 
 # --- NODE BINARY SETUP --- #
 
-NODE=https://github.com/kadena-io/chainweb-node/releases/download/1.3.1/chainweb.8.6.5.ubuntu-18.04.1e6c76b2.tar.gz
+NODE=https://github.com/kadena-io/chainweb-node/releases/download/1.4/chainweb.8.6.5.ubuntu-18.04.b7d7faa9.tar.gz
 MINER=https://github.com/kadena-io/chainweb-miner/releases/download/v1.0.3/chainweb-miner-1.0.3-ubuntu-18.04.tar.gz
 
 decho 'Downloading Node...'
 mkdir -p /root/kda
 cd /root/kda/
 wget --no-check-certificate $NODE >> $LOG_FILE 2>&1
-tar -xvf chainweb.8.6.5.ubuntu-18.04.1e6c76b2.tar.gz >> $LOG_FILE 2>&1
+tar -xvf chainweb.8.6.5.ubuntu-18.04.b7d7faa9.tar.gz >> $LOG_FILE 2>&1
 wget --no-check-certificate $MINER >> $LOG_FILE 2>&1
 tar -xvf chainweb-miner-1.0.3-ubuntu-18.04.tar.gz >> $LOG_FILE 2>&1
 
